47b207d512644fa2ca6f5fe9e9a1a9f6c1d28704
Verners Grikitis <vernersg@gmail.com>
2023-08-11 16:03:19 +0300

    added empty project

---
+++ src/test/java/io/codelex/registrationform/RegistrationFormApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codelex.registrationform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
